fbeb21391ee1f161284a5599663281a9c47cfd08
Shishir Jakati <shishir.jakati98@gmail.com>
2018-01-14 19:46:30 -0500

    Finish alexnet implementation; need to test if it works

--- keras_frcnn/alexnet.py
+++ keras_frcnn/alexnet.py
@@ -16,6 +16,8 @@ from keras import backend as K
 from keras_frcnn.RoiPoolingConv import RoiPoolingConv
 
 
+
+
 def nn_base(input_tensor=None, trainable=False):
 
 
@@ -40,20 +42,48 @@ def nn_base(input_tensor=None, trainable=False):
 
 
     ## Conv Block
-    x = Conv2D(filters=48, kernel_size=(5,5), input_shape=(32, 32, 3), activation=relu, data_format='channels_last')(img_input)
+    x = Conv2D(filters=96, kernel_size=(5,5), input_shape=(32, 32, 3), activation='relu', trainable=trainable)(img_input)
     #x = keras.layers.BatchNormalization())
-    x = Conv2D(filters=256, kernel_size=(5,5), activation='relu', strides=(2,2))(x)
+    x = Conv2D(filters=256, kernel_size=(5,5), activation='relu', strides=(2,2), trainable=trainable)(x)
     #x = keras.layers.BatchNormalization())
-    x = Conv2D(filters=384, kernel_size=(3,3), activation='relu', strides=(2,2))(x)
-    x = Conv2D(filters=384, kernel_size=(3,3), activation='relu')(x)
-    x = Conv2D(filters=256, kernel_size=(3,3), activation='relu')(x)
+    x = Conv2D(filters=384, kernel_size=(3,3), activation='relu', strides=(2,2), trainable=trainable)(x)
+    x = Conv2D(filters=384, kernel_size=(3,3), activation='relu', trainable=trainable)(x)
+    x = Conv2D(filters=256, kernel_size=(3,3), activation='relu', trainable=trainable)(x)
     
-    ## TODO: figure out whether or not the dense layers go here or not lol
-    x = Flatten()(x)
-    x = Dense(4096, activation='relu')(x)
-    x = Dense(4096, activation='relu')(x)
-    x = Dense(4096, activation='relu')(x)
-    
+    return x
+
+def rpn(base_layers, num_anchors):
+
+    ## Regional Proposal Network
+    x = Conv2D(256, (3,3), padding='SAME', activation='relu', kernel_initializer='normal', name='rpn_conv1')(base_layers)
+
+    x_class = Conv2D(num_anchors, (1, 1), activation='sigmoid', kernel_initializer='uniform', name='rpn_out_class')(x)
+    x_regr = Conv2D(num_anchors * 4, (1, 1), activation='linear', kernel_initializer='zero', name='rpn_out_regress')(x)
+
+    return [x_class, x_regr, base_layers]
+
+def classifier(base_layers, input_rois, num_rois, nb_classes=21, trainable=False):
+
+    if K.backend() == 'tensorflow':
+        pooling_regions = 7
+        input_shape = (num_rois, 7,7,256)
+    elif K.backend() == 'theano':
+        pooling_regions = 7
+        input_shape = (num_rois, 256, 7, 7)
+
+    out_roi_pool = RoiPoolingConv(pooling_regions, num_rois)([base_layers, input_rois])
+
+    out = TimeDistributed(Flatten(name='flatten'))(out_roi_pool)
+    out = TimeDistributed(Dense(4096, activation='relu', name='fc1'))(out)
+    out = TimeDistributed(Dropout(0.5))(out)
+    out = TimeDistributed(Dense(4096, activation='relu', name='fc2'))(out)
+    out = TimeDistributed(Dropout(0.5))(out)
+
+    out_class = TimeDistributed(Dense(nb_classes, activation='softmax', kernel_initializer='zero'), name='dense_class_{}'.format(nb_classes))(out)
+    # note: no regression target for bg class
+    out_regr = TimeDistributed(Dense(4 * (nb_classes-1), activation='linear', kernel_initializer='zero'), name='dense_regress_{}'.format(nb_classes))(out)
+
+    return [out_class, out_regr]
     
     
 
